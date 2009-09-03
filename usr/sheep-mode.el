0b400dafa1e0767a97ea74a24d4ba833a0c2edbe
Johannes Weiner <hannes@cmpxchg.org>
2009-09-03 21:45:21 +0200

    usr: beef up sheep-mode.el

--- usr/sheep-mode.el
+++ usr/sheep-mode.el
@@ -2,17 +2,54 @@
 ;;;
 ;;; Copyright (c) 2009 Johannes Weiner <hannes@cmpxchg.org>
 
-(require 'generic)
+;;;###autoload
+(defun sheep-mode ()
+  "Major mode for editing sheep files."
+  (interactive)
+  (kill-all-local-variables)
+  (setq major-mode 'sheep-mode
+	mode-name "Sheep")
+  (set (make-local-variable 'indent-line-function)
+       'sheep-indent-function)
+  (set-syntax-table sheep-mode-syntax-table)
+  (set (make-local-variable 'comment-start)
+       "#")
+  (set (make-local-variable 'comment-start-skip)
+       "#+ *")
+  (set (make-local-variable 'comment-end)
+       "")
+  (setq font-lock-defaults
+	'((sheep-font-lock-keywords)
+	  nil nil nil
+	  beginning-of-defun
+	  (font-lock-mark-block-function . mark-defun))))
 
-(define-generic-mode 'sheep-mode
-  ()
-  (list "quote" "block" "with" "variable" "function" "if")
-  ()
-  ;(list "ddump" "cons" "list" "head" "tail" "map")
-  (list "\\.sheep\\'")
-  (list 'sheep-mode-init)
-  "Major mode for editing Sheep files.")
+(defun sheep-indent-function ()
+  'noindent)
 
-(defun sheep-mode-init ())
+(defvar sheep-mode-syntax-table
+  (let ((table (make-syntax-table))
+	(i 0))
+    (while (< i 256)
+      (modify-syntax-entry i "_   " table)
+      (setq i (1+ i)))
+    ;; Whitespace
+    (modify-syntax-entry ?\t "    " table)
+    (modify-syntax-entry ?\n ">   " table)
+    (modify-syntax-entry ?\f "    " table)
+    (modify-syntax-entry ?\r "    " table)
+    (modify-syntax-entry ?\s "    " table)
+    ;; Comment
+    (modify-syntax-entry ?\# "<   " table)
+    ;; String
+    (modify-syntax-entry ?\" "\"   " table)
+    ;; Parenthesis
+    (modify-syntax-entry ?\( "()  " table)
+    (modify-syntax-entry ?\) ")(  " table)))
+
+(defvar sheep-font-lock-keywords
+  `(,(rx symbol-start
+	 (or "quote" "block" "if" "with" "variable" "set" "function")
+	 symbol-end)))
 
 (provide 'sheep-mode)
